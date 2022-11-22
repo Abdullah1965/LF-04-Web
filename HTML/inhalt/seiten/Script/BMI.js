2defb6b22ac051a9fadd6c199c8b272dce305165
abdullah <abdullah94@schule.bremen.de>
2022-11-22 09:38:45 +0100

    hi

--- HTML/inhalt/seiten/Script/BMI.js
+++ HTML/inhalt/seiten/Script/BMI.js
@@ -2,7 +2,6 @@ let BMI = () => {
     let cm = parseInt(document.getElementById("cm").value);
     let kg = parseInt(document.getElementById("kg").value);
     let erg = document.getElementById("erg");
-
     if (!isNaN(cm) && !isNaN(kg)) {
         let x = kg / Math.pow((cm / 100), 2);
 
@@ -12,5 +11,4 @@ let BMI = () => {
         erg.value = "Bitte geben sie die Größe und das Gewicht ein";
         erg.style.color = "red";
     }
-
 }
